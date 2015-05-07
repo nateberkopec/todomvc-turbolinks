2a6079c3ad7546d70504fd7f724d06e23ab482c4
Nate Berkopec <nate.berkopec@gmail.com>
2015-05-07 14:20:58 -0400

    Root to todos#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
+  root "todos#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
