3f4d0921ffa5d121dd2d8f6df605bc1b0162297b
Nate Berkopec <nate.berkopec@gmail.com>
2015-05-12 10:24:56 -0400

    Simplify some controller actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,56 +1,32 @@
 class TodosController < ApplicationController
   # GET /todos
-  # GET /todos.json
   def index
     @todos = Todo.all.order(created_at: :asc)
   end
 
   # POST /todos
-  # POST /todos.json
   def create
-    @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to todos_url }
-      else
-        format.html { render :index }
-      end
-    end
+    Todo.create(todo_params)
+    redirect_to todos_url
   end
 
   # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
   def update
-    @todo = Todo.find(params[:id])
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to todos_url }
-      else
-        format.html { render :edit }
-      end
-    end
+    todo = Todo.find_by(id: params[:id])
+    # cheat - sometimes the blur event handler asks to update an already destroyed record.
+    todo.try(:update, todo_params)
+    redirect_to todos_url
   end
 
   def update_many
-    @todos = Todo.where(id: params[:ids])
-    respond_to do |format|
-      if @todos.update(todo_params)
-        format.html { redirect_to todos_url }
-      else
-        format.html { render :edit }
-      end
-    end
+    Todo.where(id: params[:ids]).update(todo_params)
+    redirect_to todos_url
   end
 
   # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
-    @todo = Todo.find(params[:id])
-    @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url }
-    end
+    Todo.find(params[:id]).destroy
+    redirect_to todos_url
   end
 
   private
