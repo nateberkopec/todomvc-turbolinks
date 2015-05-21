99536699317f5c7da0ec51709afc6dcd7467d1c8
Nate Berkopec <nate.berkopec@gmail.com>
2015-05-20 21:06:20 -0400

    Optimize controller to avoid 301 redirects

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,42 +1,37 @@
 class TodosController < ApplicationController
   # GET /todos
   def index
-    @todos = Todo.belonging_to(session_user).order(created_at: :asc)
-
-    filtering_params.each do |key, value|
-      @todos = @todos.public_send(key, value) if value.present?
-    end
+    load_todos
   end
 
   # POST /todos
   def create
     Todo.belonging_to(session_user).create(todo_params)
-
-    redirect_to todos_url, change: "todos"
+    load_and_render_index
   end
 
   # PATCH/PUT /todos/1
   def update
     # cheat - sometimes the blur event handler asks to update an already destroyed record.
-    todo = Todo.belonging_to(session_user).find_by(id: params[:id]).try(:update, todo_params)
-    redirect_to todos_url, change: "todos"
+    Todo.belonging_to(session_user).where(id: params[:id]).update_all(todo_params)
+    load_and_render_index
   end
 
   def update_many
-    Todo.belonging_to(session_user).where(id: params[:ids]).update(todo_params)
-    redirect_to todos_url, change: "todos"
+    Todo.belonging_to(session_user).where(id: params[:ids]).update_all(todo_params)
+    load_and_render_index
   end
 
   # DELETE /todos/1
   def destroy
     # same problem as on update - sometimes we try to destroy twice in the JS
     Todo.belonging_to(session_user).find_by(id: params[:id]).try(:destroy)
-    redirect_to todos_url, change: "todos"
+    load_and_render_index
   end
 
   def destroy_many
     Todo.belonging_to(session_user).where(id: params[:ids]).try(:destroy_all)
-    redirect_to todos_url, change: "todos"
+    load_and_render_index
   end
 
   private
@@ -49,4 +44,17 @@ class TodosController < ApplicationController
       params.slice(:completed)
     end
     helper_method :filtering_params
+
+    def load_and_render_index
+      load_todos
+      render :index, change: "todos"
+    end
+
+    def load_todos
+      @todos = Todo.belonging_to(session_user).order(created_at: :asc)
+
+      filtering_params.each do |key, value|
+        @todos = @todos.public_send(key, value) if value.present?
+      end
+    end
 end
