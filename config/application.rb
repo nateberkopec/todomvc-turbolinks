6b320f421f0032f8f9ad411039cf70734b96955e
Nate Berkopec <nate.berkopec@gmail.com>
2015-05-22 16:20:49 -0400

    Revert "Rack-zippy"
    Turns out this gem does nothing post Rails 4.2
    
    https://github.com/rails/rails/pull/16466
    
    This reverts commit 31f3f85c33b5ab157a54ad310742b42ba3e43dc2.

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,5 @@ module Todomvc
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    # http://guides.rubyonrails.org/caching_with_rails.html
-    config.cache_store = :memory_store
   end
 end
