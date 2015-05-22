62c05089e0d4cb3523df937ece1d6b2cd2c21380
Nate Berkopec <nate.berkopec@gmail.com>
2015-05-22 17:14:14 -0400

    Insert Rack::Deflater higher in the stack

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module Todomvc
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.middleware.use Rack::Deflater
   end
 end
