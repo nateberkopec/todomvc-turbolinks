ddc7be229832cf9cfc4656d32b567cbe777cd0a0
Nate Berkopec <nate.berkopec@gmail.com>
2016-01-19 14:21:49 -0500

    Update config settings for Rails 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.static_cache_control = "public, max-age=31536000"
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
